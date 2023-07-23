75c07a264e1e518cc5e89163c08fc81fc95b11a2
Aniket Patel <patelaniket1207@gmail.com>
2023-07-24 00:09:46 +0530

    Added services section

--- Components/Services.js
+++ Components/Services.js
@@ -1,17 +1,58 @@
-import styles from '../styles/Services.module.css';
+import styles from "../styles/Services.module.css";
 
 const Services = () => {
-    return ( 
-        <>
-            <div className={styles.Services}>
-                <section className={styles.service_container}>
-                    <hr />
-                    <h4>Our Services</h4>
-                    <h1>A ONE-STOP SITE FOR ALL YOUR DIGITAL MARKETING NEEDS.</h1>
-                </section>
-            </div>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <div className={styles.Services}>
+        <div className={styles.service_container}>
+          <hr />
+          <h4>Our Services</h4>
+          <h1>A ONE-STOP SITE FOR ALL YOUR DIGITAL MARKETING NEEDS.</h1>
+        </div>
+        <section className={styles.services_list}>
+          <section className={styles.services_quote}>
+            <hr />
+            <h4>Your Perfect Brand</h4>
+          </section>
+          <section className={styles.services_list1}>
+            <hr />
+            <ul>
+              <li>
+                {" "}
+                <p>video Production</p>{" "}
+              </li>
+              <li>
+                <hr />
+                <p>Public Relations</p>{" "}
+              </li>
+              <li>
+                <hr />
+                <p>Website Design</p>
+              </li>
+            </ul>
+          </section>
+          <section className={styles.services_list2}>
+            <hr />
+            <ul>
+              <li>
+                {" "}
+                <p>Digital Marketing</p>{" "}
+              </li>
+              <li>
+                {" "}
+                <hr />
+                <p>Publicity</p>{" "}
+              </li>
+              <li>
+                <hr />
+                <p>Website Management</p>
+              </li>
+            </ul>
+          </section>
+        </section>
+      </div>
+    </>
+  );
+};
+
 export default Services;
